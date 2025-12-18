4ccd2859770b1889d6729badbdf20f73626d1f6b
Andrew Young <yandrew993@gmail.com>
2025-12-17 16:20:16 -0800

    new Model

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import testimonialRoutes from './routes/testimonial.js';
+import testimonialAndAndrewRoutes from './routes/testimonial.js';
 
 dotenv.config();
 
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '2mb' }));
 
-app.use('/api/testimonials', testimonialRoutes);
+app.use('/api/testimonials', testimonialAndAndrewRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
